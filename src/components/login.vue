b8fc6ab96d008f938f590dae544466bd5c20a5f6
wufan5098 <wufan5098@hotmail.com>
2022-12-22 17:57:51 +0800

    登陆组件完成

--- src/components/login.vue
+++ src/components/login.vue
@@ -13,20 +13,22 @@
       </el-form-item>
       <el-form-item label="密码" prop="password">
         <el-input
+          @keyup.enter.native="submit"
           v-model="form.password"
           placeholder="请输入密码"
           type="password"
         ></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">登录</el-button>
-        <el-button @click="resetForm('form')">重置</el-button>
+        <el-button type="primary" @click="submit">登录</el-button>
+        <el-button @click="resetForm">重置</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
     return {
@@ -53,8 +55,43 @@ export default {
   },
 
   methods: {
-    resetForm(formName) {
-      this.$refs[formName].resetFields()
+    // 重置
+    resetForm() {
+      this.$refs.form.resetFields()
+    },
+    // 提交
+    submit() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          axios({
+            url: 'http://localhost:8888/api/private/v1/login',
+            method: 'post',
+            data: this.form
+          }).then(res => {
+            console.log(res.data)
+            if (res.data.meta.status === 200) {
+              this.$message({
+                showClose: true,
+                message: res.data.meta.msg,
+                type: 'success',
+                duration: 2000
+              })
+              localStorage.setItem('token', res.data.data.token)
+              this.$router.push('/home')
+            } else {
+              this.$message({
+                showClose: true,
+                message: res.data.meta.msg,
+                type: 'error',
+                duration: 3000
+              })
+            }
+          })
+        } else {
+          console.log('校验没通过')
+          return false
+        }
+      })
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/login.vue'
+import Home from '@/components/home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -13,6 +14,19 @@ export default new Router({
     {
       path: '/login',
       component: Login
+    },
+    {
+      path: '/home',
+      component: Home
     }
   ]
 })
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  if (token || to.path === '/login') {
+    next()
+  } else {
+    next('/login')
+  }
+})
+export default router
